6e4948b82bca3d66b4cd35e69ec990acd057aa49
tomohiro <to.tomo.tomohiro@gmail.com>
2018-09-10 23:34:19 +0900

    add user column

--- db/migrate/20180910141826_create_users.rb
+++ db/migrate/20180910141826_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+    add_column :users, :name, :string
+    add_column :users, :gender, :integer
+    add_column :users, :birthday, :date
+    add_column :users, :mailaddress, :string    	
+    add_column :users, :password, :string    	
 
       t.timestamps
     end
