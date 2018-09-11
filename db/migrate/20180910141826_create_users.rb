73c71dcdc784a5a2f24dc51d9d0e513bac5322fe
tomohiro <to.tomo.tomohiro@gmail.com>
2018-09-11 21:32:48 +0900

    change deta type of name in user model

--- db/migrate/20180910141826_create_users.rb
+++ db/migrate/20180910141826_create_users.rb
@@ -1,12 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-    	
-    	t.integer :name, null: false
+
+    	t.string :name
     	t.integer :gender
     	t.date :birthday
-    	t.string :mailaddress, null: false
-    	t.string :password, null: false
+    	t.string :mailaddress
+    	t.string :password
 
       t.timestamps
     end
