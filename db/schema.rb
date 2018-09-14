287f289344a72a7f654711cbb30afe0370c49de7
tomohiro <to.tomo.tomohiro@gmail.com>
2018-09-14 12:30:47 +0800

    rollback and create user model

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,11 @@
 ActiveRecord::Schema.define(version: 2018_09_10_141826) do
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "gender"
+    t.date "birthday"
+    t.string "mailaddress"
+    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
