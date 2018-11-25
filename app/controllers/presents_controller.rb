0002c1ce66620cfef038c35817b0ac3c1e136d84
tomohiro <to.tomo.tomohiro@gmail.com>
2018-11-25 21:50:40 +0900

    edit create present model unfinished

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -3,12 +3,10 @@ class PresentsController < ApplicationController
 
 	def create
 	  @present = Present.new(
-	  	message: params[:present][:message],
-	  	present_opponent: params[:present][:present_opponent],
+	  	message: params[:message],
 	  	)
-	  puts @present.present_opponent
 	  puts @present.message
-	  
+	  @present.save
 	  redirect_to products_user_confirm_path
 
 	end
