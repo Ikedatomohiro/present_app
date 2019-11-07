1498fb0e0b7dc3559f2024e031672205ccebeeab
tomohiro <to.tomo.tomohiro@gmail.com>
2019-11-07 20:59:30 +0900

    herokuデプロイ設定

--- db/migrate/20180916133008_create_presents.rb
+++ db/migrate/20180916133008_create_presents.rb
@@ -1,8 +1,8 @@
 class CreatePresents < ActiveRecord::Migration[5.2]
   def change
     create_table :presents do |t|
-      t.references :user, foreign_key: true
-      t.references :present_opponent, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :present_opponent_id, foreign_key: true
       t.integer :purpose_number
       t.date :present_date
       t.date :purchase_date
