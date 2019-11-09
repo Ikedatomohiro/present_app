0c2a80248f22e5203a69487e566b483fb4f77add
tomohiro <to.tomo.tomohiro@gmail.com>
2019-11-09 17:22:54 +0900

    Cart.countのエラー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,5 @@ before_action :set_cart_all
       if signed_in?
         @carts = @carts.get_by_user_id current_user.id
       end
-    # @cartCount = @carts.count
-    @cartCount = @carts.count
   end
 end
--- db/migrate/20181106063326_create_carts.rb
+++ db/migrate/20181106063326_create_carts.rb
@@ -1,8 +1,8 @@
 class CreateCarts < ActiveRecord::Migration[5.2]
   def change
     create_table :carts do |t|
-      t.references :user, foreign_key: true
-      t.references :product, foreign_key: true
+      t.integer :user_id, foreign_key: true
+      t.integer :product_id, foreign_key: true
       t.timestamps
     end
   end
