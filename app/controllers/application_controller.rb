6c13641274706d046d91682673b289597b3c6973
tomohiro <to.tomo.tomohiro@gmail.com>
2019-11-13 22:32:02 +0900

    @cartの検索文を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ before_action :set_cart_all
   def set_cart_all
     @carts = Cart.all
       if signed_in?
-        @carts = @carts.get_by_user_id current_user.id
+        # @carts = @carts.get_by_user_id current_user.id
+        @carts = @carts.find_by(id: current_user.id)
       end
   end
 end
