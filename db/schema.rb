4247946a50a4ff447f63ec9a04085f3f2b2799ee
t-ikeda <t-ikeda@threet.co.jp>
2019-11-18 12:45:04 +0900

    windows用の設定

--- db/schema.rb
+++ db/schema.rb
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_07_02_111634) do
   end
 
   create_table "genders", force: :cascade do |t|
+    t.integer "gender"
     t.string "gender_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
