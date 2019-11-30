cbade583962b291d58afed9a36e00fb1c832f2fd
tomohiro <to.tomo.tomohiro@gmail.com>
2019-11-30 23:00:12 +0900

    herokuのパスワード再設定のバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,43 +6,43 @@ devise_for :users, controllers: {
 }
 # herokuデプロイ時のトップページ設定
 
-get "/introduction"        => "home#introduction"
-root 'home#index'
+get    "/introduction"                 => "home#introduction"
+root   'home#index'
 
-get "/" => "home#index"
+get    "/"                             => "home#index"
 
-get "products/index"        => "products#index"
-get "products/new"          => "products#new"
-get "products/:id"          => "products#show"
-post "products/create"      => "products#create"
-get "products/:id/edit"     => "products#edit"
-post "products/:id/update"  => "products#update"
-post "products/:id/destroy" => "products#destroy"
-delete "/product/dissapear" => "products#dissapear"
+get    "products/index"                => "products#index"
+get    "products/new"                  => "products#new"
+get    "products/:id"                  => "products#show"
+post   "products/create"               => "products#create"
+get    "products/:id/edit"             => "products#edit"
+post   "products/:id/update"           => "products#update"
+post   "products/:id/destroy"          => "products#destroy"
+delete "/product/dissapear"            => "products#dissapear"
+get    "products/user/purchase"        => "users#purchase"
+get    "products/user/settlement"      => "users#settlement"
+get    "products/user/confirm"         => "users#confirm"
+get    "products/user/thanks"          => "users#thanks"
+post   "products/user/present"         => "users#present"
+post   "products/user/presentopponent" => "users#presentopponent"
+delete "products/user/:id"             => "users#cartitemdelete"
+#post "products/user/settlement"     => "users#settlement"
+post   "/products/user/settlement"     => "presents#back"
+
+resources :shop
+get    "shop/stock_management/:id"     => "shop#stock_management"
+post   "shop/add_stock/:id"            => "shop#add_stock"
+get    "shop/sales_results/:id"        => "shop#sales_results"
+
+
+resources :presents
+post   "presents/set_present"          => "presents#set_present"
+get    "present/:authenticity"         => "presents#raty"
+patch  "present/:authenticity"         => "presents#update"
 
 resources :purpose
 resources :users
 resources :carts
-resources :shop
-
-resources :presents
-post "presents/set_present"      => "presents#set_present"
-post "/products/user/settlement" => "presents#back"
-
-get "products/user/purchase"         => "users#purchase"
-get "products/user/settlement"       => "users#settlement"
-get "products/user/confirm"          => "users#confirm"
-get "products/user/thanks"           => "users#thanks"
-post "products/user/present"         => "users#present"
-post "products/user/presentopponent" => "users#presentopponent"
-delete "products/user/:id"           => "users#cartitemdelete"
-#post "products/user/settlement"     => "users#settlement"
-get "shop/stock_management/:id" => "shop#stock_management"
-post "shop/add_stock/:id" => "shop#add_stock"
-get "present/:authenticity"  => "presents#raty"
-patch "present/:authenticity"  => "presents#update"
-
-get "shop/sales_results/:id" => "shop#sales_results"
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
